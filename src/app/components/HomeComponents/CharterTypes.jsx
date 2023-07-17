df1b98997028278fb2077307cc712e3f2a98717b
gabewolford <gabewolford@gmail.com>
2023-07-17 11:48:02 -0700

    created charter types component

---
+++ src/app/components/HomeComponents/CharterTypes.jsx
@@ -0,0 +1,62 @@
+import Link from "next/link"
+import Image from "next/image"
+
+export default function CharterTypes() {
+    return (
+        <>
+            <div className="mx-6 lg:mx-20 mb-16">
+                    <h2 className="text-2xl medium mb-6">Types of charters</h2>
+                    <div className="flex flex-col md:flex-row md:grid md:grid-cols-3 md:flex-wrap md:gap-6 items-center self-stretch">
+                        <div className="flex items-center md:flex-col self-stretch mb-6 gap-4">
+                            <div className="w-2/5 md:w-auto">
+                                <Image
+                                    src="/images/backcountry-image-home.png"
+                                    height={357}
+                                    width={357}
+                                    alt="Backcountry snook picture"
+                                >
+                                </Image>
+                            </div>
+                            <div className="flex flex-col items-start w-3/5 md:w-full gap-2">
+                                <h3 className="text-base md:text-lg medium">Backcountry & Inshore</h3>
+                                <p className="text-md">Snook, redfish, tarpon and more</p>
+                                <Link href="/charters/inshore-backcountry" className="text-cyan-420 hover:text-cyan-hover transition duration-100 ease-in-out text-md medium">Explore Inshore</Link>
+                            </div>
+                        </div>
+                        <div className="flex items-center md:flex-col self-stretch mb-6 gap-4">
+                            <div className="w-2/5 md:w-auto">
+                                <Image
+                                    src="/images/offshore-image-home.png"
+                                    height={357}
+                                    width={357}
+                                    alt="Offshore mahi picture"
+                                >
+                                </Image>
+                            </div>
+                            <div className="flex flex-col items-start w-3/5 md:w-full gap-2">
+                                <h3 className="text-base md:text-lg medium">Reef & Offshore</h3>
+                                <p className="text-md">Sailfish, wahoo, tuna and more</p>
+                                <Link href="/charters/offshore" className="text-cyan-420 hover:text-cyan-hover transition duration-100 ease-in-out text-md medium">Explore Offshore</Link>
+                            </div>
+                        </div>
+                        <div className="flex items-center md:flex-col self-stretch md:mb-6 gap-4">
+                            <div className="w-2/5 md:w-auto">
+                                <Image
+                                    src="/images/ecotours-image-home.png"
+                                    height={357}
+                                    width={357}
+                                    alt="Eco Tours Alligator Lighthouse picture"
+                                >
+                                </Image>
+                            </div>
+                            <div className="flex flex-col items-start w-3/5 md:w-full gap-2">
+                                <h3 className="text-base md:text-lg medium">Eco & Enviro Tours</h3>
+                                <p className="text-md">Backcountry and beach trips</p>
+                                <Link href="/charters/eco-enviro-tours" className="text-cyan-420 hover:text-cyan-hover transition duration-100 ease-in-out text-md medium">Explore Eco Tours</Link>
+                            </div>
+                        </div>
+                    </div>
+            </div>
+        </>
+    )
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
 import Hero from './components/HomeComponents/Hero'
+import CharterTypes from './components/HomeComponents/CharterTypes'
 
 export default function HomePage() {
   return (
     <>
       <Hero />
+      <CharterTypes />
     </>
   )
 }
