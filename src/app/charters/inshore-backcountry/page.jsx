247228780be47f72094e9cc127f36b5e341aaba8
gabewolford <gabewolford@gmail.com>
2023-07-17 14:36:29 -0700

    backcountry components created and refactored

--- src/app/charters/inshore-backcountry/page.jsx
+++ src/app/charters/inshore-backcountry/page.jsx
@@ -1,6 +1,47 @@
+'use client'
+
+import { useEffect } from "react"
+import Breadcrumbs from "../../components/ChartersComponents/Backcountry/Breadcrumbs"
+import PhotoLeftCard1 from "../../components/ChartersComponents/Backcountry/PhotoLeftCard1"
+import PhotoRightCard2 from "../../components/ChartersComponents/Backcountry/PhotoRightCard2"
+import PhotoLeftCard3 from "../../components/ChartersComponents/Backcountry/PhotoLeftCard3"
+import PhotoRightCard4 from "../../components/ChartersComponents/Backcountry/PhotoRightCard4"
+
 export default function InshoreBackcountryPage() {
+
+    useEffect(() => {
+        const cards = document.querySelectorAll('.fade-in')
+        const observer = new IntersectionObserver(entries => {
+            entries.forEach(entry => {
+                entry.target.classList.toggle('show', entry.isIntersecting)
+                if (entry.isIntersecting) {
+                    observer.unobserve(entry.target)
+                }
+            })
+        },
+        {
+            threshold: .25,
+        }
+        )
+    
+        cards.forEach(card => {
+            observer.observe(card)
+        })
+    }, [])
+
     return (
         <>
+            <div className="mx-auto max-w-7xl pt-16 md:pt-24 animate__animated animate__fadeIn">
+                <div className="mx-6 lg:mx-20 mb-0 md:mb-8">
+                    <Breadcrumbs />
+                    <PhotoLeftCard1 />
+                    <PhotoRightCard2 />
+                    <PhotoLeftCard3 />
+                    <PhotoRightCard4 />
+                </div>
+            </div>
+            
+    
         </>
-    )
+    )      
 }
---
+++ src/app/components/ChartersComponents/Backcountry/PhotoRightCard2.jsx
@@ -0,0 +1,16 @@
+import Link from 'next/link'
+
+export default function PhotoRightCard2() {
+    return (
+        <div className="fade-in flex flex-col md:flex-row items-center gap-6 lg:gap-28 order-1 mb-16">
+            <img className="md:w-1/2 md:order-1" src="/images/bonefish.png" alt="Bonefish Image" />
+            <div className="flex flex-col md:order-0 lg:ml-20">
+                <h4 className="text-2xl md:text-3xl medium leading-7 mb-4 ">Bonefishing on the Flats</h4>
+                <p className="text-sm md:text-base mb-4">Stalk or hunt across the shallow flats with your guide for the elusive bonefish. Also known as the “Grey Ghost”, this tough catch is one of the most sought-after challenges for anglers. Bonefishing is year-round in the Florida Keys.</p>
+                <p className="text-sm md:text-base bold mb-4">Availability: All year</p>
+                <p className="text-xs mb-6">Rates vary depending on the boat and number of people.</p>
+                <Link href="/contact" className="text-white medium bg-cyan-420 hover:bg-cyan-hover transition duration-300 ease-in-out rounded-3xl px-5 py-3.5 w-fit">Book A Charter</Link>
+            </div>
+        </div>
+    )
+}
