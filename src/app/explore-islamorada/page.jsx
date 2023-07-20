c60a864688bde6d502980160ef451fd71c211396
gabewolford <gabewolford@gmail.com>
2023-07-20 16:32:51 -0700

    built resort grabber for sanity, refactored resort card and explore page

--- src/app/explore-islamorada/page.jsx
+++ src/app/explore-islamorada/page.jsx
@@ -1,97 +1,25 @@
-'use client'
-
-import { useEffect, useState } from "react"
-import ResortCard from '../components/ExploreComponents/ResortCard'
 import Breadcrumbs from "../components/ExploreComponents/Breadcrumbs"
 import PhotoLeftCard from "../components/ExploreComponents/PhotoLeftCard"
-import WhereToStay from "../components/ExploreComponents/WhereToStay"
+import GetResorts from "../api/GetResorts"
+
+export const metadata = {
+  title: "Islamorada Fishing Guides | Explore Islamorada",
+  icons: {
+    icon: "/images/tan-favicon.png"
+  },
+  description: "Experience the ultimate Florida Keys fishing adventure. Fish with the experts of Islamorada in the backcountry waters of the Florida Bay or the offshore blue waters of the Gulf Stream.",
+  keywords: "florida keys, islamorada, florida, fishing, sport fishing, sportfishing, guides, charters, fishing charters, fishing guides, tours, florida bay, gulf stream, inshore, backcountry, offshore, reef, tarpon, sailfish, bonefish, snook, permit, redfish, trout, mahi-mahi, dorado, dolphin, grouper, snapper, hogfish, barracuda, sharks, everglades, sandbar, flats, alligator lighthouse, cape sable"
+}
 
 export default function ExploreIslamoradaPage() {
-    const [resorts, setResorts] = useState([])
-
-    const allResorts = [
-      {
-        name: 'Amara Cay',
-        blurb: 'Amara Cay Resort is anchored in the laid-back style and carefree atmosphere that makes Islamorada the Sport Fishing Capital of the World.',
-        main_photo: '/images/resorts/amara-cay-main.png',
-        logo: '/images/resorts/amara-cay-logo.png',
-        url: 'https://www.islamoradaresortcollection.com/amara-cay-resort',
-      },
-      {
-        name: 'Cheeca Lodge',
-        blurb: 'Cheeca Lodge & Spa has been an exclusive hideaway for celebrities, business leaders and U.S. Presidents for over 70 years.',
-        main_photo: '/images/resorts/cheeca-lodge-main.png',
-        logo: '/images/resorts/cheeca-lodge-logo.png',
-        url: 'https://www.cheeca.com/',
-      },
-      {
-        name: 'Playa Largo',
-        blurb: 'Located in Key Largo, FL, Playa Largo Resort & Spa, Autograph Collection offers 178 luxury rooms, suites, bungalows, and a beach house.',
-        main_photo: '/images/resorts/playa-largo-main.png',
-        logo: '/images/resorts/playa-largo-logo.png',
-        url: 'https://www.playalargoresort.com/',
-      },
-      {
-        name: 'Fisher Inn',
-        blurb: 'The Fisher Inn Resort & Marina offers guests a modern, stylish, and comfortable place to stay while visiting Islamorada.',
-        main_photo: '/images/resorts/fisher-inn-main.png',
-        logo: '/images/resorts/fisher-inn-logo.png',
-        url: 'https://www.fisherinnresort.com/',
-      },
-      {
-        name: 'The Islands of Islamorada',
-        blurb: 'Seamlessly weaves the allure of tropical beauty with familiar luxurious comforts, creating a captivating experience for the whole family.',
-        main_photo: '/images/resorts/islands-islamorada-main.png',
-        logo: '/images/resorts/islands-islamorada-logo.png',
-        url: 'https://www.theislandsofislamorada.com/',
-      },
-      {
-        name: 'Casa Morada',
-        blurb: 'Casa Morada is an upscale sixteen suite privately owned hotel inspired by the informal character of the Caribbean.',
-        main_photo: '/images/resorts/casa-morada-main.png',
-        logo: '/images/resorts/casa-morada-logo.png',
-        url: 'https://casamorada.com/',
-      },
-  ]
-    useEffect(() => {
-        setResorts(allResorts)
-
-        const cards = document.querySelectorAll('.fade-in')
-        const observer = new IntersectionObserver(entries => {
-            entries.forEach(entry => {
-                entry.target.classList.toggle('show', entry.isIntersecting)
-                if (entry.isIntersecting) {
-                    observer.unobserve(entry.target)
-                }
-            })
-        },
-        {
-            threshold: .25,
-        }
-        )
-
-        cards.forEach(card => {
-            observer.observe(card)
-        })
-    }, [])
-
-    let resortsList
-    if (allResorts.length > 0) {
-      resortsList = resorts
-        .map((resort, i) => <ResortCard key={i} resortData={resort} />)
-    }
 
     return (
       <>
-        <head>
-            <title>Islamorada Fishing Guides | Explore Islamorada</title>
-            <link rel="icon" href="/images/tan-favicon.png"/>
-        </head>
         <div className="mx-auto max-w-7xl pt-16 md:pt-24 animate__animated animate__fadeIn">
             <div className="mx-6 lg:mx-20 mb-16">
                 <Breadcrumbs />
                 <PhotoLeftCard />
-                <WhereToStay resortsList={resortsList} />
+                <GetResorts />
             </div>
         </div>
       </>
