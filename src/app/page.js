618fc283b89325b4203f0920c9f87690707e9845
gabewolford <gabewolford@gmail.com>
2023-07-24 19:54:15 -0700

    added meta tags

--- src/app/page.js
+++ src/app/page.js
@@ -10,7 +10,7 @@ export const metadata = {
     icon: "/images/tan-favicon.png"
   },
   description: "Experience the ultimate Florida Keys fishing adventure. Fish with the experts of Islamorada in the backcountry waters of the Florida Bay or the offshore blue waters of the Gulf Stream.",
-  keywords: "florida keys, islamorada, florida, fishing, sport fishing, sportfishing, guides, charters, fishing charters, fishing guides, tours, florida bay, gulf stream, inshore, backcountry, offshore, reef, tarpon, sailfish, bonefish, snook, permit, redfish, trout, mahi-mahi, dorado, dolphin, grouper, snapper, hogfish, barracuda, sharks, everglades, sandbar, flats, alligator lighthouse, cape sable"
+  keywords: "florida keys fishing trips, florida keys fishing guides, florida keys fishing guides, islamorada fishing guides, islamorada fishing charters, florida keys, islamorada, florida, fishing, sport fishing, sportfishing, guides, charters, fishing charters, fishing guides, tours, florida bay, gulf stream, inshore, backcountry, offshore, reef, tarpon, sailfish, bonefish, snook, permit, redfish, trout, mahi-mahi, dorado, dolphin, grouper, snapper, hogfish, barracuda, sharks, everglades, sandbar, flats, alligator lighthouse, cape sable"
 }
 
 export default function HomePage() {
