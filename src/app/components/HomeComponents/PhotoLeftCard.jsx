3113200ec5659c05731973afe37cd8331a535e27
gabewolford <gabewolford@gmail.com>
2023-07-27 14:33:07 -0700

    home page components font optimized

--- src/app/components/HomeComponents/PhotoLeftCard.jsx
+++ src/app/components/HomeComponents/PhotoLeftCard.jsx
@@ -1,13 +1,14 @@
 import Link from "next/link";
+import { apercuMedium } from '../../../app/styles/fonts'
 
 export default function PhotoLeftCard() {
     return (
         <section className="fade-in show flex flex-col md:flex-row items-center gap-6 lg:gap-28 order-1 mx-6 lg:mx-20 mb-16">
             <img className="md:w-1/2" src="/images/book-directly-image-home.png" alt="Tarpon Image" />
             <div className="flex flex-col items-start lg:mr-20">
-                <h4 className="text-2xl md:text-3xl medium leading-7 mb-4 ">Book directly with one of our local guides</h4>
+                <h4 className={`text-2xl md:text-3xl leading-7 mb-4 ${apercuMedium.className}`}>Book directly with one of our local guides</h4>
                 <p className="text-sm md:text-base mb-4">All of our guides are licensed captains with years of experience and local expertise. We welcome all experience levels, whether you’re a seasoned fly fisherman or a family looking to try something new.</p>
-                <Link href="/guides" className="text-white medium bg-cyan-420 hover:bg-cyan-hover transition duration-300 ease-in-out rounded-3xl px-5 py-3.5 w-fit">Meet Our Guides</Link>
+                <Link href="/guides" className={`${apercuMedium.className} text-white bg-cyan-420 hover:bg-cyan-hover transition duration-300 ease-in-out rounded-3xl px-5 py-3.5 w-fit`}>Meet Our Guides</Link>
             </div>
         </section>
     )
--- src/app/components/HomeComponents/PhotoRightCard.jsx
+++ src/app/components/HomeComponents/PhotoRightCard.jsx
@@ -1,13 +1,14 @@
 import Link from "next/link";
+import { apercuMedium } from '../../../app/styles/fonts'
 
 export default function PhotoRightCard() {
     return (
         <section className="fade-in flex flex-col md:flex-row items-center gap-6 lg:gap-28 mx-6 lg:mx-20 order-1 mb-16">
             <img className="md:w-1/2 md:order-1" src="/images/wws-image-home.png" alt="World Wide Sportsman aerial view" />
             <div className="flex flex-col md:order-0 lg:ml-20">
-                <h4 className="text-2xl md:text-3xl medium leading-7 mb-4 ">Meet us at World Wide Sportsman</h4>
+                <h4 className={`text-2xl md:text-3xl leading-7 mb-4 ${apercuMedium.className}`}>Meet us at World Wide Sportsman</h4>
                 <p className="text-sm md:text-base mb-4">Our fishing charters begin and end at World Wide Sportsman Bayside Marina, the finest full service marina in the Florida Keys. Learn more about our marina and where to stay any play while in town.</p>
-                <Link href="/explore-islamorada" className="text-white medium bg-cyan-420 hover:bg-cyan-hover transition duration-300 ease-in-out rounded-3xl px-5 py-3.5 w-fit">Explore Islamorada</Link>
+                <Link href="/explore-islamorada" className={`text-white ${apercuMedium.className} bg-cyan-420 hover:bg-cyan-hover transition duration-300 ease-in-out rounded-3xl px-5 py-3.5 w-fit`}>Explore Islamorada</Link>
             </div>
         </section>
     )
