145a828ca5066b8d8906855f021aa23cd6b06c02
gabewolford <gabewolford@gmail.com>
2023-07-27 15:36:42 -0700

    partner components font optimized

--- src/app/components/PartnersComponents/Breadcrumbs.jsx
+++ src/app/components/PartnersComponents/Breadcrumbs.jsx
@@ -1,11 +1,12 @@
 import Link from "next/link"
+import { apercuBold } from "../../styles/fonts"
 
 export default function Breadcrumbs() {
     return (
         <>
             <div className="flex flex-col items-start gap-3 mb-4 md:mb-8">
                 <p className="text-sm"><Link href="/" className="hover:text-cyan-hover">Home</Link> / Partners</p>
-                <h1 className="text-3xl lg:text-5xl bold">Our Partners</h1>
+                <h1 className={`text-3xl lg:text-5xl ${apercuBold.className}`}>Our Partners</h1>
             </div>
         </>
     )
--- src/app/components/PartnersComponents/PartnerCard.jsx
+++ src/app/components/PartnersComponents/PartnerCard.jsx
@@ -1,9 +1,10 @@
 import Link from "next/link";
+import { apercuBold } from "../../styles/fonts";
 
 export default function PartnerCard({ partnerData }) {
     let partnerName, partnerPhoto, partnerURL;
     if (partnerData) {
-        partnerName = <h3 className="text-sm text-center bold mt-2">{partnerData.name}</h3>;
+        partnerName = <h3 className={`text-sm text-center mt-2 ${apercuBold.className}`}>{partnerData.name}</h3>;
         partnerPhoto = <img src={partnerData.imageUrl} alt={partnerData.name} className="mx-auto w-full" />;
         partnerURL = partnerData.url;
     }
