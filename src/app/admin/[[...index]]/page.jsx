6c182245ab3037019ea9fc0fe81f2ac94cdc05f6
gabewolford <gabewolford@gmail.com>
2023-08-06 14:17:16 -0700

    fixed layout for studio, added top padding

--- src/app/admin/[[...index]]/page.jsx
+++ src/app/admin/[[...index]]/page.jsx
@@ -13,5 +13,11 @@ import { NextStudio } from 'next-sanity/studio'
 import config from '../../../../sanity.config'
 
 export default function StudioPage() {
-  return <NextStudio config={config}/>
+  return (
+    <div className='pt-[68px]'>
+      <NextStudio config={config}/>
+    </div>
+  )
+  
+
 }
