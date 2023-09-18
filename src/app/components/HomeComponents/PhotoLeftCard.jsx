570e0d711f3b2fccaf8e0ff0c2cdaf37f68eef34
gabewolford <gabewolford@gmail.com>
2023-09-17 19:40:39 -0700

    optimized home images

--- src/app/components/HomeComponents/PhotoLeftCard.jsx
+++ src/app/components/HomeComponents/PhotoLeftCard.jsx
@@ -1,10 +1,17 @@
 import Link from "next/link";
 import { apercuMedium } from '../../../app/styles/fonts'
+import Image from "next/image";
+import bookDirectlyImage from '../../../../public/images/book-directly-image-home.png'
 
 export default function PhotoLeftCard() {
     return (
         <section className="fade-in show flex flex-col md:flex-row items-center gap-6 lg:gap-28 order-1 mx-6 lg:mx-20 mb-16">
-            <img className="md:w-1/2" src="/images/book-directly-image-home.png" alt="Tarpon Image" />
+            <Image 
+                src={bookDirectlyImage}
+                alt="Book directly image"
+                className="md:w-1/2"
+                placeholder="blur"
+            />
             <div className="flex flex-col items-start lg:mr-20">
                 <h4 className={`text-2xl md:text-3xl leading-7 mb-4 ${apercuMedium.className}`}>Book directly with one of our local guides</h4>
                 <p className="text-sm md:text-base mb-4">All of our guides are licensed captains with years of experience and local expertise. We welcome all experience levels, whether you’re a seasoned fly fisherman or a family looking to try something new.</p>
--- src/app/components/HomeComponents/PhotoRightCard.jsx
+++ src/app/components/HomeComponents/PhotoRightCard.jsx
@@ -1,10 +1,17 @@
 import Link from "next/link";
 import { apercuMedium } from '../../../app/styles/fonts'
+import Image from "next/image";
+import wwsImage from '../../../../public/images/wws-image-home.png'
 
 export default function PhotoRightCard() {
     return (
         <section className="fade-in flex flex-col md:flex-row items-center gap-6 lg:gap-28 mx-6 lg:mx-20 order-1 mb-16">
-            <img className="md:w-1/2 md:order-1" src="/images/wws-image-home.png" alt="World Wide Sportsman aerial view" />
+            <Image 
+                src={wwsImage}
+                alt="World Wide Sportsman aerial view"
+                className="md:w-1/2 md:order-1"
+                placeholder="blur"
+            />
             <div className="flex flex-col md:order-0 lg:ml-20">
                 <h4 className={`text-2xl md:text-3xl leading-7 mb-4 ${apercuMedium.className}`}>Meet us at World Wide Sportsman</h4>
                 <p className="text-sm md:text-base mb-4">Our fishing charters begin and end at World Wide Sportsman Bayside Marina, the finest full service marina in the Florida Keys. Learn more about our marina and where to stay any play while in town.</p>
